517ac54a9966f8e3e7b1350a2cf69ae54ee5f677
anmolpaweriya <paweriyaanmol@gmail.com>
2026-02-17 19:42:28 +0530

    added cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const puppeteer = require("puppeteer");
 const swaggerUi = require("swagger-ui-express");
@@ -6,6 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 /**
  * @swagger
